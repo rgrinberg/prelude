ff70e34a7397582cd6f7023fc59c667429507b7c
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-10-30 16:13:58 -0400

    enable cos mode in hook for elixir

--- personal/elixir.el
+++ personal/elixir.el
@@ -1,2 +1,8 @@
 
 ;; (require 'elixir-mode)
+(defun elixir-mode-compile-on-save ()
+  "Elixir mode compile files on save."
+  (and (file-exists (buffer-file-name))
+       (file-exists (elixir-mode-compiled-file-name))
+       (elixir-cos-mode t)))
+(add-hook 'elixir-mode-hook 'elixir-mode-compile-on-save)
