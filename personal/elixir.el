abc2645c68437e326700539ce917426554ec5bdb
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-11-02 14:19:48 -0400

    file-exists to file-exists-p

--- personal/elixir.el
+++ personal/elixir.el
@@ -2,7 +2,8 @@
 ;; (require 'elixir-mode)
 (defun elixir-mode-compile-on-save ()
   "Elixir mode compile files on save."
-  (and (file-exists (buffer-file-name))
-       (file-exists (elixir-mode-compiled-file-name))
+  (and (file-exists-p (buffer-file-name))
+       (file-exists-p (elixir-mode-compiled-file-name))
        (elixir-cos-mode t)))
+
 (add-hook 'elixir-mode-hook 'elixir-mode-compile-on-save)
