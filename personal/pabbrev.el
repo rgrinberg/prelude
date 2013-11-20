c23117b7d86dd7b69e6b6633b329531258328d11
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-11-20 00:39:11 -0800

    updates

--- personal/pabbrev.el
+++ personal/pabbrev.el
@@ -451,6 +451,7 @@ I'm not telling you which version, I prefer."
    dired-mode 
    eshell-mode
    slime-repl-mode
+   erc-mode
    nrepl-mode
    utop-mode
    ;; gnus article mode is read-only so should be missed anyway,
