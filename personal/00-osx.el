67785dc099fe4b9543be7b4d1f0c516afebf711e
Rudi Grinberg <rgrinberg@Rudis-MacBook-Pro.local>
2013-11-30 15:23:12 -0800

    osx fixes

---
+++ personal/00-osx.el
@@ -0,0 +1,6 @@
+;;; OSX only fixes
+;; TODO: don't load when linux
+(setq mac-command-modifier 'meta)
+(setq mac-option-modifier 'super)
+(set-default-font "Monaco 13")
+(set-variable 'magit-emacsclient-executable "/usr/local/Cellar/emacs/24.3/bin/emacsclient")
