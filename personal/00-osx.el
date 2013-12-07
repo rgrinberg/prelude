aad3b5a72d18b929b1303213b73e0deb95357443
Rudi Grinberg <rudi.grinberg@gmail.com>
2013-12-06 16:54:26 -0800

    more osx only fixes

--- personal/00-osx.el
+++ personal/00-osx.el
@@ -2,5 +2,14 @@
 ;; TODO: don't load when linux
 (setq mac-command-modifier 'meta)
 (setq mac-option-modifier 'super)
-(set-default-font "Monaco 13")
+(set-default-font "Monaco 12")
+;; this hack for magit, assumes that emacs has been installed with homebrew
+;; TODO: check that this executable exists
 (set-variable 'magit-emacsclient-executable "/usr/local/Cellar/emacs/24.3/bin/emacsclient")
+
+(setq locale-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8-unix)
+(set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+(setq system-uses-terminfo nil)
